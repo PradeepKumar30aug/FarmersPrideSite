c73bc9f6c0982bfa89c7b407f29c7ca20d87ef61
Pradeep Kumar <pradeep30aug@gmail.com>
2020-08-19 12:38:36 +0530

    Create authontication_footer.php

---
+++ include/authontication_footer.php
@@ -0,0 +1,25 @@
+ <div class="footer-top">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4">
+            <div class="social">
+              <ul>
+                <li class="fb"><a href="#"></a></li>
+                <li class="tw"><a href="#"></a></li>
+               <!--  <li class="googleplus"><a href="#"></a></li>
+                <li class="rss"><a href="#"></a></li>
+                <li class="pintrest"><a href="#"></a></li> -->
+                <li class="linkedin"><a href="#"></a></li>
+                <li class="youtube"><a href="#"></a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-sm-4 col-xs-12 coppyright"> 
+      	&copy; 2020 PILO SHUDH PANI SEVA FOUNDATION. All Rights Reserved.
+      	</div>
+          <div class="col-xs-12 col-sm-4">
+            <div class="payment-accept"> <img src="images/payment-1.png" alt=""> <img src="images/payment-2.png" alt=""> <img src="images/payment-3.png" alt=""> <img src="images/payment-4.png" alt=""> </div>
+          </div>
+        </div>
+      </div>
+    </div>
