f42fc073d56d25ad4aa0844002239f8b50ff5c7e
Pradeep Kumar <pradeep30aug@gmail.com>
2020-08-19 12:38:48 +0530

    Create footer.php

---
+++ include/footer.php
@@ -0,0 +1,56 @@
+    <div class="footer-middle">
+      <div class="container">
+        <div class="row">
+           <div class="col-md-3 col-sm-6">
+            <div class="footer-column pull-left">
+              <h4>About US</h4>
+              <ul class="links">
+                <li><a href="login.html" title="Your Account">Our products are
+                 sourced from some of the best organic farmers in various villages
+                  in India. The products are known for their excellent quality and 
+                taste. We are NABL certified organic brand of India.</a></li>                
+              </ul>
+            </div>
+          </div>     
+
+
+          <div class="col-md-3 col-sm-6">
+            <div class="footer-column pull-left">
+              <h4>Shopping Guide</h4>
+              <ul class="links">
+                <li><a href="blog.html" title="How to buy">Blog</a></li>
+                <li><a href="faq.html" title="FAQs">FAQs</a></li>
+                <li><a href="#" title="Payment">Payment</a></li>
+                <li><a href="#" title="Shipment">Shipment</a></li>
+                <li><a href="#" title="Where is my order?">Where is my order?</a></li>
+                <li><a href="#" title="Return policy">Return policy</a></li>
+              </ul>
+            </div>
+          </div>
+          
+          <div class="col-md-3 col-sm-6">
+            <div class="footer-column pull-left">
+              <h4>Information</h4>
+              <ul class="links">
+                <li><a href="sitemap.html" title="Site Map">Site Map</a></li>
+                <li><a href="#" title="Search Terms">Search Terms</a></li>
+                <li><a href="#" title="Advanced Search">Advanced Search</a></li>
+                <li><a href="about_us.html" title="About Us">About Us</a></li>
+                <li><a href="contact_us.html" title="Contact Us">Contact Us</a></li>
+                <li><a href="#" title="Suppliers">Suppliers</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <h4>Contact Us</h4>
+            <div class="contacts-info">
+<address><i class="add-icon"></i>Plot 1 & 2, Tower A, Sector 125, Noida<br>
+Uttar Pradesh , 201301<br>
+</address>
+<div class="phone-footer"><i class="phone-icon"></i>+91 7987551332, +91 7291990128</div>
+<div class="email-footer"><i class="email-icon"></i><a href="mailto:abc@example.com">farmerspride27@gmail.com</a></div>
+</div>
+          </div>
+        </div>
+      </div>
+    </div>
