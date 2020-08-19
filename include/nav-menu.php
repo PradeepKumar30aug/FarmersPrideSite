e04ec4b1845d5674beb8d9b8f977401379415ebe
Pradeep Kumar <pradeep30aug@gmail.com>
2020-08-19 12:39:03 +0530

    Create nav-menu.php

---
+++ include/nav-menu.php
@@ -0,0 +1,71 @@
+ <nav class="hidden-xs">
+      <div class="nav-container">
+        <div class="col-md-3 col-xs-12 col-sm-3">
+          <div class="mega-container visible-lg visible-md visible-sm">
+            <div class="navleft-container">
+            
+              <div class="mega-menu-title"> <div class="glow">&nbsp;</div>
+                <h3><i class="fa fa-navicon"></i> All Categories</h3>
+              </div>
+              <div class="mega-menu-category">
+                <ul class="nav">
+                   <li class="nosub"> <a href="index-2.html">Gift Boxes</a></li>
+                                     <!-- <li> <a href="#">Pages</a>
+                    <div class="wrap-popup">
+                      <div class="popup">
+                        <div class="row">
+                          <div class="col-sm-6">
+                            <ul class="nav">
+                              <li><a href="grid.html"><span>Grid</span></a> </li>
+                              <li> <a href="list.html"> <span>List</span> </a> </li>
+                              <li> <a href="product_detail.html"> <span>Product Detail</span> </a> </li>
+                              <li> <a href="shopping_cart.html"> <span>Shopping Cart</span> </a> </li>
+                              <li><a href="checkout.html"><span>Checkout</span></a> </li>
+                              <li> <a href="wishlist.html"> <span>Wishlist</span> </a> </li>
+                            </ul>
+                          </div>
+                          <div class="col-sm-6 has-sep">
+                            <ul class="nav">
+                              <li> <a href="dashboard.html"> <span>Dashboard</span> </a> </li>
+                              <li> <a href="multiple_addresses.html"> <span>Multiple Addresses</span> </a> </li>
+                              <li><a href="compare.html"><span>Compare</span></a> </li>
+                              <li><a href="quick_view.html"><span>Quick View</span></a> </li>
+                              <li><a href="newsletter.html"><span>Newsletter</span></a> </li>
+                              <li><a href="404error.html"><span>404 Error Page</span></a> </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </li> -->
+                
+                   <li class="nosub"><a href="#">Organic Rice</a></li>
+                  <li class="nosub"><a href="#">Organic Sweets</a></li>
+                  <li class="nosub"><a href="#">Organic Pluses</a></li>
+                  <li class="nosub"><a href="#">Organic Oils</a></li>
+                  <li class="nosub"><a href="#">Organic Spices</a></li>
+                  <li class="nosub"><a href="#">Organic Millets</a></li>
+                  <li class="nosub"><a href="#">Organic Cow Ghee</a></li>
+                  <li class="nosub"><a href="#">Organic Flours</a></li>
+                  <li class="nosub custom-offer"><a href="#">Deal of the day</a></li>
+                
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- features box -->
+        <div class="our-features-box hidden-xs">
+          <div class="features-block">
+            <div class="col-lg-9 col-md-9 col-xs-12 col-sm-9 offer-block">
+               <a href="#">Products</a> 
+               <a href="#">Events</a> 
+               <a href="#">About US</a> 
+               <a href="#">Blog</a> 
+               <a href="#">Health Boxes</a> 
+               <a href="#" class="hidden-sm">Todays Deals</a>
+               <span>All India Delivery</span> </div>
+          </div>
+        </div>
+      </div>
+    </nav>
